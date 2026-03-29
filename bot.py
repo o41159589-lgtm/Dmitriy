b50905dbf3bf7d38baaa2b504da3bbb9e0745152
Sceletonchik <skelet0nch1k2025@gmail.com>
2026-03-29 16:38:30 +0300

    122

--- bot.py
+++ bot.py
@@ -28,6 +28,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 )
+
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=BOT_TOKEN)
